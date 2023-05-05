5aa11d99deb51b971e0fbd6b81f1a38e4522616a
jaypark <pjhyun1990@gmail.com>
2023-05-05 23:18:22 +0900

    config redis

---
+++ src/main/java/com/example/pricecompareredis/config/RedisConfig.java
@@ -0,0 +1,34 @@
+package com.example.pricecompareredis.config;
+
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+	private final RedisProperties redisProperties;
+
+	public RedisConfig(final RedisProperties redisProperties) {
+		this.redisProperties = redisProperties;
+	}
+
+	public RedisConnectionFactory redisConnectionFactory() {
+		return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+	}
+
+	@Bean
+	@Primary
+	public RedisTemplate<String, Object> redisTemplate() {
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(redisConnectionFactory());
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(new StringRedisSerializer());
+		return redisTemplate;
+	}
+}
